8da017785de5a7f1c1f34dbe8e1dbdfc59d573c7
Glenn Walter Lim <walterlim2001@yahoo.com>
2018-11-19 11:06:14 +0800

    Added Connection String

---
+++ Sales Inventory/Sales Inventory/ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales_Inventory
+{
+    public class ConnectionString
+    {
+        String Connection = "datasource=localhost;port=3306;username=root;password=;database=sales inventory;";
+    }
+}
